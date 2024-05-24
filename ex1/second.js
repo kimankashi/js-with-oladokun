b81c99254593ef1213fd1a275ff7a8f536b7979c
kimankashi <kimankashi6@gmail.com>
2024-05-24 10:18:42 +0100

    java

---
+++ ex1/second.js
@@ -0,0 +1,28 @@
+//1. Write a single line comment which says, comments can make code readable
+
+
+//2. Write another single comment which says, Welcome to JavaScriptWithMrOladokun
+
+
+//3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+//4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let string = "kiman"
+let boolean = true == false
+let undefined
+let nulls = null
+
+//5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof "kiman")
+console.log(typeof "20*")
+
+//6. Declare four variables with assigned values
+
+let _name ="kim"
+//7. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+//8. Declare variables to store your first name, last name, marital status, country and age in a single line
+//9. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+//I am 25 years old.
+//You are 30 years old.
+
